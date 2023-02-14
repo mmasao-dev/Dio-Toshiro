56e7a49f57fbc0833a00ea6913ab4a924c0badf3
mmasao-dev <mmasao@gmail.com>
2023-02-14 11:14:36 -0300

    correção da tabela

--- index.php
+++ index.php
@@ -13,10 +13,10 @@ header('Content-Type: text/html; charset=iso-8859-1');
 
 echo 'Versao Atual do PHP: ' . phpversion() . '<br>';
 
-$servername = "54.234.153.24";
+$servername = "44.201.33.73";
 $username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$password = "SenhaSegura";
+$database = "DB_Toshiro";
 
 // Criar conexão
 
@@ -34,11 +34,11 @@ $valor_rand2 = strtoupper(substr(bin2hex(random_bytes(4)), 1));
 $host_name = gethostname();
 
 
-$query = "INSERT INTO dados (AlunoID, Nome, Sobrenome, Endereco, Cidade, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
+$query = "INSERT INTO dados (ProdutoID, Produto, Descrição, Valor, Codigo, Host) VALUES ('$valor_rand1' , '$valor_rand2', '$valor_rand2', '$valor_rand2', '$valor_rand2','$host_name')";
 
 
 if ($link->query($query) === TRUE) {
-  echo "New record created successfully";
+  echo "Novo valor gravado com sucesso";
 } else {
   echo "Error: " . $link->error;
 }
