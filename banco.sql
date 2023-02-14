db990821dbaca8e3df8248163d701541fdc815ea
mmasao-dev <mmasao@gmail.com>
2023-02-14 11:11:39 -0300

    atualização banco

--- banco.sql
+++ banco.sql
@@ -1,8 +1,8 @@
 CREATE TABLE dados (
-    AlunoID int,
-    Nome varchar(50),
-    Sobrenome varchar(50),
-    Endereco varchar(150),
-    Cidade varchar(50),
+    ProdutoID int,
+    Produto varchar(50),
+    Descrição varchar(50),
+    Valor int,
+    Codigo varchar(50),
     Host varchar(50)
 );
